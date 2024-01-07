68ec8798389b0462f824168935e879c999ee3767
Robert Todea <robert.todea@mai.ai>
2024-01-07 10:45:16 +0200

    Add new order and refactor

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,26 +1,47 @@
+require './app/services/percent_parser'
+require './app/services/pipe_parser'
+require './app/services/dollar_parser'
+
 class PeopleController
   def initialize(params)
     @params = params
   end
 
   def normalize
-    people = PercentParser.new(params[:percent_format]).people
-    people += DollarParser.new(params[:dollar_format]).people
-    people += PipeParser.new(params[:pipe_format]).people
-
-    people.sort! do |person_a, person_b|
-      person_a.first_name <=> person_b.first_name
-    end
-
-    result = []
-    people.each do |person|
-      result << person.to_s
-    end
-
-    result
+    fetch_people_from_all_parsers(params.slice(:percent_format,
+                                               :dollar_format,
+                                               :pipe_format))
+      .then { |people| sort_people(people, params[:order]) }
+      .map(&:to_s)
   end
 
   private
 
   attr_reader :params
+
+  PARSERS = {
+    percent_format: PercentParser,
+    dollar_format: DollarParser,
+    pipe_format: PipeParser
+  }.freeze
+
+  def fetch_people_from_all_parsers(inputs)
+    people = []
+
+    PARSERS.each do |format, parser_klass|
+      people += parser_klass.new(params[format]).people if inputs[format]
+    end
+
+    people
+  end
+
+  def sort_people(people, by)
+    comparisons = {
+      birthdate: ->(a, b) { a.birthdate <=> b.birthdate },
+      first_name: ->(a, b) { a.first_name <=> b.first_name }
+    }
+    sort_lambda = comparisons.fetch(by, ->(_a, _b) { 0 })
+
+    people.sort(&sort_lambda)
+  end
 end
